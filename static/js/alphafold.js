930b3d674ef2a9f52e18b20263e8f6f8f483921a
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-10-08 16:45:22 +1000

    Add some styling back in

--- static/js/alphafold.js
+++ static/js/alphafold.js
@@ -8,7 +8,7 @@ window.addEventListener( "resize", function( event ){
 
 // Load PDB entry 1CRN
 stage.loadFile(
-  "rcsb://1crn",
-  // "https://raw.githubusercontent.com/neoformit/alphafold-galaxy/main/data/ranked_0.pdb",
+  // "rcsb://1crn",
+  "https://raw.githubusercontent.com/neoformit/alphafold-galaxy/main/data/ranked_0.pdb",
   { defaultRepresentation: true }
 );
